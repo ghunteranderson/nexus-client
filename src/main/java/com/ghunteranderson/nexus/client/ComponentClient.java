e5780734b0e7efcacfc216477ff6539c2b9b13b5
G. Hunter Anderson <ghunteranderson@gmail.com>
2019-12-30 21:18:44 -0600

    Adding component client

--- src/main/java/com/ghunteranderson/nexus/client/ComponentClient.java
+++ src/main/java/com/ghunteranderson/nexus/client/ComponentClient.java
@@ -1,12 +1,46 @@
 package com.ghunteranderson.nexus.client;
 
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Stream;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
 
 import com.ghunteranderson.nexus.model.Component;
 
 public class ComponentClient {
 
-	public Stream<Component> find(){
-		return null;
+	private final WebTarget client;
+	
+	public ComponentClient(NexusInstance instance) {
+		client = instance.getWebTarget("/service/rest/v1/components");
+	}
+	
+	public Stream<Component> findAll(String repository){
+		Function<String, PaginatedResponse<Component>> source = token -> {
+			return client
+					.queryParam("repository", repository)
+					.queryParam("continuationToken", token)
+					.request()
+					.accept(MediaType.APPLICATION_JSON)
+					.get(new GenericType<PaginatedResponse<Component>>() {});
+		};
+		
+		return new PaginationIterator<>(source).stream();
+	}
+	
+	public Optional<Component> find(String id){
+		try {
+			return Optional.of(client.path(id).request().get(Component.class));
+		} catch(NotFoundException ex) {
+			return Optional.empty();
+		}
+	}
+	
+	public void delete(String id) {
+		client.path(id).request().delete();
 	}
 }
